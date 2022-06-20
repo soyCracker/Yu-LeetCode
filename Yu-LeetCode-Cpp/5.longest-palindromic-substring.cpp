c5a2a68684c0b3ff70163d04e23e05039055fa41
Luka Lai <lukalai@starlux-airlines.com>
2022-06-20 14:23:26 +0800

    2個字元也找得到，但算太久

--- Yu-LeetCode-Cpp/5.longest-palindromic-substring.cpp
+++ Yu-LeetCode-Cpp/5.longest-palindromic-substring.cpp
@@ -18,39 +18,52 @@ public:
             for(int j=i-1;j>=0;j--)
             {              
                 temp+=s[j];
+
+                if(max.length()>=(temp.length()*2))
+                {
+                    continue;
+                }
+
+                if(max.length()<3 && j==i-1 && temp[0]==temp[1])
+                {
+                    max=temp;
+                }
+                
                 if(i+1+temp.length()<=s.length())
                 {
                     string sub2 = s.substr(i+1,temp.length());
                     if(sub2.find(temp)!= std::string::npos)
                     {
                         string test = s.substr(i-(temp.length()-1), temp.length()*2);
-                        if(test.length()>max.length())
-                        {
-                            max = test;
-                        }
+                        max = GetBiggerStr(max, test);
                     }
                 }
+
                 if(i+temp.length()<=s.length())
                 {
                     string sub1 = s.substr(i,temp.length());
                     if(sub1.find(temp)!= std::string::npos)
                     {
                         string test = s.substr(i-(temp.length()-1), temp.length()*2-1);
-                        if(test.length()>max.length())
-                        {
-                            max = test;
-                        }
+                        max = GetBiggerStr(max, test);
                     }
                 }
                 else
                 {
                     break;
                 }
-
-                if()
             }         
         }
         return max;
+    }
+
+    string GetBiggerStr(string a, string b)
+    {
+        if(a.length()>=b.length())
+        {
+            return a;
+        }
+        return b;
     }
 };
 // @lc code=end
