2bb8f8f485a39b6ced05276aa1209febdc74de1a
Luka Lai 賴育暉 <lukalai@starlux-airlines.com>
2023-07-11 14:55:45 +0800

    feat(6): 應該是夠快了，只是用無意義的2維陣列太花時間，已修復

--- src/6.zigzag-conversion.cs
+++ src/6.zigzag-conversion.cs
@@ -7,38 +7,21 @@
 // @lc code=start
 public class Solution {
     public string Convert(string s, int numRows)
-    {
-        //int lines = (s.Length-numRows)/(numRows-1)+1;
-        //int nodes = (s.Length-numRows)%(numRows-1);
-        //int bevels = (lines-1)/(numRows-1)+1;
-
-        if (numRows == 1)
-        {
-            return s;
-        }
-
-        int strLen = s.Length;
-        int width = GetWidth(strLen, numRows);
-
-        //初始化table
-        char[,] table = new char[numRows, width];
+    {      
+        string[] arr = new string[numRows];
         for (int i = 0; i < numRows; i++)
         {
-            for (int j = 0; j < width; j++)
-            {
-                table[i, j] = '#';
-                //Console.Write("char:" + table[i, j]);
-            }
+            arr[i] = "";
         }
 
         //計算各字元位置
+        int strLen = s.Length;
         int count = 0;
         int r = 0;
-        int w = 0;
         bool bevel = false;
         while (count < strLen)
         {
-            table[r, w] = s[count];
+            arr[r] = $"{arr[r]}{s[count]}";
             count++;
             if (count == strLen)
             {
@@ -59,10 +42,9 @@ public class Solution {
             if (bevel && r > 0)
             {
                 r--;
-                w++;
             }
 
-            if (bevel && r == 0)
+            if (r == 0)
             {
                 bevel = false;
             }
@@ -72,35 +54,9 @@ public class Solution {
         string res = "";
         for (int i = 0; i < numRows; i++)
         {
-            for (int j = 0; j < width; j++)
-            {
-                //Console.Write(table[i, j]);
-                if (table[i, j] != '#')
-                {
-                    res += table[i, j];
-                }
-            }
+            res = $"{res}{arr[i]}";
         }
         return res;
-    }
-
-    private int GetWidth(int strLen, int numRows)
-    {
-        if (strLen <= numRows)
-        {
-            return 1;
-        }
-
-        //X  X  X  X X X X X
-        //X XX XX XX XXXXXX
-        //XX XX XX X X X X
-        //X  X  X  X
-
-        int branch = (strLen - numRows) / (numRows - 1);
-        int even = branch % 2;
-        int bevels = even == 0 ? branch / 2 : branch / 2 + 1;
-        int nodes = (strLen - numRows) % (numRows - 1);
-        return bevels * (numRows - 1) + nodes + 1;
     }
 }
 // @lc code=end
